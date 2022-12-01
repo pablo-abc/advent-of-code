197639d9a5a619c6693ed42ce7dc0e1680d85660
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-12-01 17:33:01 +0100

    feat: initial commit

---
+++ 2022/1/init.lua
@@ -0,0 +1,18 @@
+local io = require('io')
+local table = require('table')
+
+local totals = { 0 }
+
+for line in io.lines() do
+  if line == '' then
+    totals[#totals + 1] = 0
+  else
+    local c = tonumber(line)
+    totals[#totals] = c + totals[#totals]
+  end
+end
+
+table.sort(totals, function(a, b) return a > b end)
+
+print('Part 1:', totals[1])
+print('Part 2:', totals[1] + totals[2] + totals[3])
